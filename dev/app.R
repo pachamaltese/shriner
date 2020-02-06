8bbad0d7eeb7bbd0ba4c3e2e0cb4dd992e887ec1
Pachamaltese <mvargas@dcc.uchile.cl>
2020-02-06 09:56:41 -0300

    darker left side

--- dev/app.R
+++ dev/app.R
@@ -1,5 +1,6 @@
 ## app.R ##
 library(shriner)
+library(highcharter)
 
 ui <- dashboardPage(
   dashboardHeader(
@@ -39,15 +40,15 @@ ui <- dashboardPage(
   ),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+      menuItem("Base plot", tabName = "baseplot", icon = icon("dashboard")),
+      menuItem("Highcharter plot", tabName = "highcharterplot", icon = icon("th"))
     )
   ),
   ## Body content
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "dashboard",
+      tabItem(tabName = "baseplot",
               fluidRow(
                 box(h1("This is a title"), width = 12),
 
@@ -61,8 +62,27 @@ ui <- dashboardPage(
       ),
 
       # Second tab content
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+      tabItem(tabName = "highcharterplot",
+              fluidRow(
+                box(h1("This is another title"), width = 12),
+
+                box(
+                  selectInput("type", label = "Type", width = "100%",
+                              choices = c("line", "column", "bar", "spline")),
+                  selectInput("stacked", label = "Stacked",  width = "100%",
+                              choices = c(FALSE, "normal", "percent")),
+                  selectInput("theme", label = "Theme",  width = "100%",
+                              choices = c(FALSE, "fivethirtyeight", "economist",
+                                          "darkunica", "gridlight", "sandsignika",
+                                          "null", "handdrwran", "chalk")
+                  )
+                ),
+
+                box(
+                  title = "Highcharter output",
+                  highchartOutput("plot2",height = "500px")
+                )
+              )
       )
     )
   )
@@ -75,6 +95,37 @@ server <- function(input, output) {
   output$plot1 <- renderPlot({
     data <- histdata[seq_len(input$slider)]
     hist(data)
+  })
+
+  output$plot2 <- renderHighchart({
+
+    hc <- highcharts_demo() %>%
+      hc_rm_series("Berlin") %>%
+      hc_chart(type = input$type)
+
+    if (input$stacked != FALSE) {
+      hc <- hc %>%
+        hc_plotOptions(series = list(stacking = input$stacked))
+    }
+
+    if (input$theme != FALSE) {
+      theme <- switch(input$theme,
+                      null = hc_theme_null(),
+                      darkunica = hc_theme_darkunica(),
+                      gridlight = hc_theme_gridlight(),
+                      sandsignika = hc_theme_sandsignika(),
+                      fivethirtyeight = hc_theme_538(),
+                      economist = hc_theme_economist(),
+                      chalk = hc_theme_chalk(),
+                      handdrwran = hc_theme_handdrawn()
+      )
+
+      hc <- hc %>% hc_add_theme(theme)
+
+    }
+
+    hc
+
   })
 }
 
